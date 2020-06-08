18b964d5ae789d360b2e1e31e5147135d1be0fb5
Annie Huang <annie.hn@gmail.com>
2020-06-08 13:52:05 +1000

    Make the whole thing grow and shrink

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const container = document.querySelector('.container');
 const text = document.querySelector('#text');
 
 const totalTime = 7500;
-const breatheTime = (totalTime / 5) * 2;
-const holdTime = totalTime / 5;
+const breatheTime = (totalTime / 5) * 2;    // 3s
+const holdTime = totalTime / 5;             // 1.5s
 
 breatheAnimation();
 
